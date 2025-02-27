c727fc418fb840091347d09014b88ebe169808e7
Jules624 <hgobrnt@gmail.com>
2025-02-27 14:32:55 +0100

    Feat: ContactForm, backend

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="bg-gray-800 text-white p-4 text-center">
-            <p>© 2025 La Belle Grume</p>
-        </footer>
-    );
-}
-
-export default Footer;
--- src/site/About.js
+++
@@ -1,5 +0,0 @@
-function About() {
-    return <h1>À propos</h1>
-}
-
-export default About;
